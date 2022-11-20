8460e0e7cde5bb914c471fcea341a83431276640
Zloysisk <zloysisk@gmail.com>
2022-11-20 21:03:13 +0300

    Update Test.py
    Commit

--- Test.py
+++ Test.py
@@ -6,7 +6,12 @@ for card_count in range(1, continer + 1):
         print("Неравельно указан вес контейнера : не больше 200 кг, не может быть отрицательным ")
         continue
     list_container_weights.append(continer_num)
+
 new_container_weight = int(input("Введите вес нового контейнера: "))
 
-list_container_weights.append(new_container_weight)
+for count, weights in enumerate(list_container_weights): 
+    if weights > new_container_weight: 
+        list_container_weights.insert(count, new_container_weight)
+
+
 print(list_container_weights)
